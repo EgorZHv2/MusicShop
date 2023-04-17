52925895144ded0868b56ab26b6f70bad6943312
Егор <egor.zholobov@yandex.ru>
2023-04-17 15:40:40 +0300

    Add product properties controller

---
+++ MusicShop.WebAPI/Controllers/ProductPropertyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicShop.Application.DTO.PageModels;
+using MusicShop.Application.Interfaces.Services;
+
+namespace MusicShop.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductPropertyController : ControllerBase
+    {
+        private readonly IProductPropertyService _productPropertyService;
+        public ProductPropertyController(IProductPropertyService productPropertyService)
+        {
+            _productPropertyService= productPropertyService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetPage([FromQuery]PaginationDTO dto)
+        {
+            var result = await _productPropertyService.GetPage(dto);
+            return Ok(result);
+        }
+    }
+}
