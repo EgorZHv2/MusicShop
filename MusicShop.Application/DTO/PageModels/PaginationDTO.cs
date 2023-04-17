8a8e76de9a6cc8089f6a0875c5b282a25be3e1d8
Егор <egor.zholobov@yandex.ru>
2023-04-17 15:38:16 +0300

    Add pagination

---
+++ MusicShop.Application/DTO/PageModels/PaginationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicShop.Application.DTO.PageModels
+{
+    public class PaginationDTO
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
