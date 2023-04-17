c2b59e151136219c177b5acd86da333c3ae45cc3
Егор <egor.zholobov@yandex.ru>
2023-04-17 15:39:32 +0300

    Add product properties service

---
+++ MusicShop.Application/Interfaces/Services/IProductPropertyService.cs
@@ -0,0 +1,23 @@
+﻿using MusicShop.Application.DTO.PageModels;
+using MusicShop.Application.DTO.ProductProperty;
+using MusicShop.Application.Interfaces.Repositories;
+using MusicShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Application.Interfaces.Services
+{
+    public interface IProductPropertyService
+    {
+        Task<Guid> Create(CreateProductPropertyDTO dto);
+
+        Task<List<OutputShortProductPropertyDTO>> GetShortList();
+
+        Task Delete(Guid id);
+
+        Task<PageModelDTO<OutputPageProductPropertyDTO>> GetPage(PaginationDTO paginationDTO);
+    }
+}
--- MusicShop.Application/Services/ProductPropertyService.cs
+++ MusicShop.Application/Services/ProductPropertyService.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using AutoMapper;
+using MusicShop.Application.DTO.ProductProperty;
+using MusicShop.Domain.Entities;
+using MusicShop.Application.Interfaces.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MusicShop.Application.DTO.PageModels;
+using MusicShop.Application.Interfaces.Services;
 
 namespace MusicShop.Application.Services
 {
-    public class ProductPropertyService
+    public class ProductPropertyService:IProductPropertyService
     {
+        private readonly IProductPropertyRepository _productPropertyRepository;
+        private readonly IMapper _mapper;
+
+        public ProductPropertyService(IProductPropertyRepository productPropertyRepository
+            ,IMapper mapper)
+        {
+            _productPropertyRepository = productPropertyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Guid> Create(CreateProductPropertyDTO dto)
+        {
+            var entity = _mapper.Map<ProductPropertyEntity>(dto);
+           
+            var result = await _productPropertyRepository.Create(entity);
+            if (dto.ValueType == Domain.Enums.PropertyValueType.Set && dto.Values.Any())
+            {
+                foreach (var item in dto.Values)
+                {
+                    entity.ProductPropertySet.Add(new ProductPropertySetEntity
+                    {
+                        Id = Guid.NewGuid(),
+                        ProductPropertyId = result,
+                        Value = item
+                    });
+                }
+                await _productPropertyRepository.Update(entity);
+            }
+            return result;
+        } 
+   
+        public async Task<List<OutputShortProductPropertyDTO>> GetShortList()
+        {
+            var entities = await _productPropertyRepository.GetAll();
+            var result = _mapper.Map<List<OutputShortProductPropertyDTO>>(entities);
+            return result;
+        }
+        public async Task Delete(Guid id)
+        {
+            var entity = await _productPropertyRepository.GetById(id);
+            if (entity != null)
+            await _productPropertyRepository.Delete(entity);
+        }
+
+        public async Task<PageModelDTO<OutputPageProductPropertyDTO>> GetPage(PaginationDTO paginationDTO)
+        {
+            var pages = await _productPropertyRepository.GetPage(paginationDTO);
+            var result = _mapper.Map<PageModelDTO<OutputPageProductPropertyDTO>>(pages);
+            return result;
+        }
     }
 }
